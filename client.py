a2399a4ac626c3a1be25a549a74a240861cd590c
ThatMG393 <thatmg393@gmail.com>
2023-09-07 16:44:42 +0800

    General improvements, formatted files using autopep8, black, yapf, added python docs using pyment

--- client.py
+++ client.py
@@ -4,36 +4,51 @@
 # This is a client file, this connects to the server
 
 import socket
-import datetime
 import struct
-import os
-import time
 import threading
+import sys
 
 
-# Annotations
-def threaded(fn):
+class ThreadExt(threading.Thread):
+    """Threading class that has a `stop()` function
+    Your code should regularly check if the thread is running using `is_running`
+    """
 
-    def wrapper(*args, **kwargs) -> threading.Thread:
-        thread = threading.Thread(target=fn, args=args, kwargs=kwargs)
-        thread.start()
-        return thread
+    def __init__(self, target, args: tuple = ()) -> None:
+        super(ThreadExt, self).__init__(target=target, args=args)
+        self.is_running = False
+        self.daemon = True
 
-    return wrapper
+    def start(self) -> None:
+        """Starts the thread"""
+        self.is_running = True
+        super().start()
+
+    def stop(self) -> None:
+        """Stops the thread"""
+        self.is_running = False
 
 
 # Based on osid alary `senrev` class
 # This is mostly it but formatted
 class SockSendRecv:
+    """Class to automatically handle sending and receiving bytes in packets"""
 
     def __init__(self, sock: socket.socket) -> None:
         self.sock = sock
 
     def send(self, data: bytes) -> None:
+        """
+        Sends data to the socket
+
+        :param data: bytes:
+
+        """
         packet = struct.pack(">I", len(data)) + data
         self.sock.sendall(packet)
 
     def recv_packet(self) -> bytes:
+        """Receive bytes as a packet"""
         packet_len = self.recv(4)
         if not packet_len:
             return b""
@@ -42,9 +57,19 @@ class SockSendRecv:
         return self.recv(packet_len)
 
     def recv(self, n: int) -> bytes:
+        """
+        Receive `n` amount of bytes
+
+        :param n: int:
+
+        """
         packet = b""
 
-        while len(packet) < n:
+        while (
+            getattr(
+                threading.current_thread(),
+                "is_running",
+                True) and len(packet) < n):
             frame = self.sock.recv(n - len(packet))
             if not frame:
                 return b""
@@ -54,14 +79,22 @@ class SockSendRecv:
         return packet
 
     def wait_for_packet(self, packet: bytes) -> bool:
-        while True:
+        """
+        Wait for specified packet to arrive
+
+        :param packet: bytes:
+        :returns bool:
+
+        """
+        while getattr(threading.current_thread(), "is_running", True):
             recv_packet = self.recv_packet()
             if recv_packet == packet:
                 return True
 
+        return False
+
 
 class Logger:
-
     def __init__(self, tag: str) -> None:
         self.tag = tag
 
@@ -76,18 +109,28 @@ class Logger:
 
 
 class Client:
+    """Client of the remote shell server"""
 
     def __init__(self) -> None:
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock_senrev = SockSendRecv(self.client_socket)
-        self.logger = Logger(":".join(
-            str(elem) for elem in self.client_socket.getsockname()))
+        self.logger = Logger(
+            ":".join(str(elem) for elem in self.client_socket.getsockname())
+        )
 
-        self.server_msg_running = False
+        self.server_msg_recieving = False
         self.block_input = False
         self.server_cwd = "?"
+        self.server_msg_thread = ThreadExt(self.server_msg_recv)
 
     def connect(self, ip: str, port: int) -> None:
+        """
+        Connects to the server
+
+        :param ip: str:
+        :param port: int:
+
+        """
         try:
             self.client_socket.connect((ip, port))
 
@@ -98,6 +141,7 @@ class Client:
                 self.logger.info("Server requires authuentication")
                 token = input("Token: ").strip()
                 self.sock_senrev.send(token.encode())
+                
                 if self.sock_senrev.recv_packet() == "/granted".encode():
                     self.logger.info("Success!")
                     welcome_packet = self.sock_senrev.recv_packet()
@@ -108,10 +152,17 @@ class Client:
             if welcome_packet == "/connected".encode():
                 self.logger.info("Received connected packet")
                 self.sock_senrev.send("/received_connected".encode())
+                self.server_cwd = (
+                    self.sock_senrev.recv_packet()
+                    .decode("utf-8")
+                    .replace("/cwd_changed", "", 1)
+                    .strip()
+                )
+
                 self.start()
             else:
                 self.logger.err("Server did not send a connected packet...")
-                exit(1)
+                self.stop(1)
         except (ConnectionResetError, ConnectionRefusedError):
             self.logger.err(
                 f"Can't connect to {ip}:{port} is the server is running?")
@@ -120,46 +171,63 @@ class Client:
             self.stop()
 
     def start(self) -> None:
-        self.server_msg_running = True
-        self.server_msg()
-        self.main()
+        """Starts the shell and server message reciever"""
+        self.server_msg_thread.start()
+        self.shell()
 
-    def stop(self) -> None:
-        self.server_msg_running = False
+    def stop(self, code: int = 0) -> None:
+        """Stops the shell and server message reciever"""
+        self.server_msg_thread.stop()
+        self.sock_senrev.send("/exit".encode())
+        exit(code)
 
-    def main(self) -> None:
+    def shell(self) -> None:
+        """A make-up shell used for sending commands to the server"""
         self.logger.info("Starting shell...")
         print()
 
-        while self.server_msg_running:
+        while self.server_msg_thread.is_running:
             if not self.block_input:
-                command = input(f"[{self.server_cwd}]: ").strip()
+                command = input(f"{self.server_cwd} $ ").strip()
                 if command:
-                    if command == "/exit":
-                        self.sock_senrev.send(command.encode())
+                    if command == "/exit" or command == "exit":
+                        self.sock_senrev.send("/exit".encode())
                         self.stop()
                     else:
                         self.sock_senrev.send(command.encode())
                         self.block_input = True
 
-    @threaded
-    def server_msg(self) -> None:
-        self.logger.info("Starting server message handler thread")
-        while self.server_msg_running:
-            server_message = self.sock_senrev.recv_packet().decode("utf-8")
-            if server_message:
-                if server_message == "/end_block_input":
-                    self.block_input = False
-                elif "/cwd_changed" in server_message:
-                    self.server_cwd = server_message.replace(
-                        "/cwd_changed", "", 1).strip()
-                elif server_message == "/server_shutdown":
-                    print("Server shutting down!")
-                    self.stop()
-                else:
-                    print(server_message)
+    def server_msg_recv(self) -> None:
+        """Handles messages from the server"""
+        self.logger.info("Starting server message reciever thread")
+
+        while self.server_msg_thread.is_running:
+            try:
+                server_message = self.sock_senrev.recv_packet().decode("utf-8")
+                if server_message:
+                    if server_message == "/end_block_input":
+                        self.block_input = False
+                    elif "/cwd_changed" in server_message:
+                        self.server_cwd = server_message.replace(
+                            "/cwd_changed", "", 1
+                        ).strip()
+                    elif server_message == "/server_conn_shutdown":
+                        self.logger.info("Server shutting down!")
+                        self.stop()
+                    else:
+                        print(server_message)
+            except OSError as e:
+                if "Errno 9" in str(e):
+                    self.logger.err(
+                        "Connection to server has been unexpectedly closed..."
+                    )
+                    self.stop(1)
 
 
 if __name__ == "__main__":
-    client = Client()
-    client.connect("localhost", 6969)
+    if len(sys.argv) != 3:
+        print(f"{sys.argv[0]} requires 2 argument got {len(sys.argv) - 1}")
+        print(f"{sys.argv[0]}: <ip> <port>")
+    else:
+        client = Client()
+        client.connect(str(sys.argv[1]), int(sys.argv[2]))
--- server.py
+++ server.py
@@ -12,58 +12,90 @@ import threading
 import subprocess
 import random
 import string
+import sys
 from concurrent.futures import Future
 
 
 # Global functions
-def gen_token(size=24) -> str:
-    generated = "".join([
-        random.choice(string.ascii_uppercase + string.ascii_lowercase +
-                      string.digits) for n in range(size)
-    ])
+def gen_token(size: int = 24) -> str:
+    """
+    Generates token with specified size
+    :param size:  (Default value = 24)
+
+    """
+    generated = "".join(
+        [
+            random.choice(
+                string.ascii_uppercase + string.ascii_lowercase + string.digits
+            )
+            for n in range(size)
+        ]
+    )
     return generated
-
-
-def call_with_future(fn, future, args, kwargs) -> None:
-    try:
-        result = fn(*args, **kwargs)
-        future.set_result(result)
-    except Exception as exc:
-        future.set_exception(exc)
 
 
 # Annotations
 def threaded(fn):
+    """
+    Marks a function threaded
 
-    def wrapper(*args, **kwargs) -> Future:
-        future = Future()
-        threading.Thread(target=call_with_future,
-                         args=(fn, future, args, kwargs)).start()
-        return future
+    :param fn:
+
+    """
+
+    def wrapper(*args, **kwargs):
+        thread = threading.Thread(target=fn, args=args, kwargs=kwargs)
+        thread.daemon = True
+        thread.start()
 
     return wrapper
 
 
 SERVER_REQUIRE_AUTH = True
-if SERVER_REQUIRE_AUTH:
-    SERVER_TOKEN = gen_token()
-    print(f"This is the server token: {SERVER_TOKEN}")
-    print("New token is generated each server restart.")
-    print()
+SERVER_TOKEN = os.getenv("REMSHELL_TOKEN")
+DEFAULT_CWD = os.getcwd()
+
+
+class ThreadExt(threading.Thread):
+    """Threading class that has a `stop()` function
+    Your code should regularly check if the thread is running using `is_running()`
+    """
+
+    def __init__(self, target, args: tuple = ()) -> None:
+        super(ThreadExt, self).__init__(target=target, args=args)
+        self.is_running = False
+        self.daemon = True
+
+    def start(self) -> None:
+        """Starts the thread"""
+        self.is_running = True
+        super().start()
+
+    def stop(self) -> None:
+        """Stops the thread"""
+        self.is_running = False
 
 
 # Based on osid alary `senrev` class
 # This is mostly it but formatted
 class SockSendRecv:
+    """Class to automatically handle sending and receiving bytes in packets"""
 
     def __init__(self, sock: socket.socket) -> None:
         self.sock = sock
 
     def send(self, data: bytes) -> None:
+        """
+        Sends data to the socket
+
+        :param data: bytes:
+
+        """
         packet = struct.pack(">I", len(data)) + data
         self.sock.sendall(packet)
 
     def recv_packet(self) -> bytes:
+        """Receive bytes as a packet"""
         packet_len = self.recv(4)
         if not packet_len:
             return b""
@@ -72,9 +104,19 @@ class SockSendRecv:
         return self.recv(packet_len)
 
     def recv(self, n: int) -> bytes:
+        """
+        Receive `n` amount of bytes
+
+        :param n: int:
+
+        """
         packet = b""
 
-        while len(packet) < n:
+        while (
+            getattr(
+                threading.current_thread(),
+                "is_running",
+                True) and len(packet) < n):
             frame = self.sock.recv(n - len(packet))
             if not frame:
                 return b""
@@ -84,14 +126,21 @@ class SockSendRecv:
         return packet
 
     def wait_for_packet(self, packet: bytes) -> bool:
-        while True:
+        """
+        Wait for specified packet to arrive
+
+        :param packet: bytes:
+        :returns bool:
+
+        """
+        while getattr(threading.current_thread(), "is_running", True):
             recv_packet = self.recv_packet()
             if recv_packet == packet:
                 return True
+        return False
 
 
 class Logger:
-
     def __init__(self, tag: str) -> None:
         self.tag = tag
 
@@ -106,19 +155,23 @@ class Logger:
 
 
 class Server:
+    """Server of the remote shell"""
 
     def __init__(self, ip: str, port: int) -> None:
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.setsockopt(
+            socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         try:
             self.server_socket.bind((ip, port))
         except OSError as e:
             if "Errno 98" in str(e):
                 print(f"An application is using the current port '{port}'")
+                exit(1)
 
         self.server_clients = []
 
     def start(self) -> None:
+        """Starts the server and listen for incoming clients"""
         self.server_socket.listen(1)
         print(
             f"Server running at { ':'.join(str(elem) for elem in self.server_socket.getsockname()) } on { datetime.datetime.now().strftime('%c') }"
@@ -133,12 +186,24 @@ class Server:
         except KeyboardInterrupt:
             print("Catching interrupt and exiting")
             for client in self.server_clients:
-                client.stop()
+                try:
+                    client.stop()
+                except Exception:
+                    print("Caught an exception while shutting down, ingoring...")
 
             self.server_socket.shutdown(socket.SHUT_RDWR)
             self.server_socket.close()
+            exit(0)
 
-    def new_client(self, client: socket.socket, addr) -> None:
+    def new_client(self, client: socket.socket, addr: tuple[str, int]) -> None:
+        """
+        Creates a new `ServerClient` then append it
+        to the `self.server_clients` list
+
+        :param client: socket.socket:
+        :param addr: tuple[str, int]:
+
+        """
         server_client = ServerClient(client, addr)
         self.server_clients.append(server_client)
 
@@ -146,19 +211,20 @@ class Server:
 
 
 class ServerClient:
+    """The client of the `Server`"""
 
-    def __init__(self, client_socket: socket.socket, client_addr) -> None:
+    def __init__(
+        self, client_socket: socket.socket, client_addr: tuple[str, int]
+    ) -> None:
         self.logger = Logger(f"{client_addr[0]}:{client_addr[1]}")
 
         self.client_socket = client_socket
         self.sock_senrev = SockSendRecv(client_socket)
-        self.current_wd = os.getcwd()
+        self.current_wd = DEFAULT_CWD
+        self.client_thread = ThreadExt(self.client_msg_handler)
 
-        self.is_running = False
-        self.is_connected = False
-
-    @threaded
     def auth(self) -> bool:
+        """Authuenticate with the client"""
         self.sock_senrev.send("/req_auth".encode())
         token = self.sock_senrev.recv_packet().decode("utf-8")
 
@@ -172,138 +238,167 @@ class ServerClient:
 
     @threaded
     def connect(self) -> None:
-        if not self.is_connected:
-            if SERVER_REQUIRE_AUTH:
-                self.logger.info("Asking for authuentication")
-                if self.auth().result():
-                    self.logger.info("Authuenticated!")
-                else:
-                    self.client_socket.close()
-                    return
-
-            self.logger.info("Sending connected packet")
-            self.sock_senrev.send("/connected".encode())
-            if self.sock_senrev.wait_for_packet("/received_connected".encode()):
-                self.logger.info("Client received connected packet")
-
-                self.logger.info("Sending current WD")
-                self.sock_senrev.send(
-                    f"/cwd_changed {self.current_wd}".encode())
-
-                self.is_connected = True
-                self.start()
+        """Connect to the client"""
+        if SERVER_REQUIRE_AUTH:
+            self.logger.info("Asking for authuentication")
+            if self.auth():
+                self.logger.info("Authuenticated!")
             else:
-                self.logger.err("Client did not respond to connected packet...")
+                self.client_socket.close()
+                return
+
+        self.logger.info("Sending connected packet")
+        self.sock_senrev.send("/connected".encode())
+        if self.sock_senrev.wait_for_packet("/received_connected".encode()):
+            self.logger.info("Client received connected packet")
+            self.is_connected = True
+
+            self.logger.info("Sending current WD")
+            self.sock_senrev.send(f"/cwd_changed {self.current_wd}".encode())
+
+            self.start()
+        else:
+            self.logger.err("Client did not respond to connected packet...")
 
     def start(self) -> None:
-        if not self.is_running and self.is_connected:
-            self.is_running = True
-            self.main()
+        """Start the client message handler"""
+        if not self.client_thread.is_running:
+            self.client_thread.start()
 
     def stop(self) -> None:
-        if self.is_running and self.is_connected:
-            self.is_running = False
-            self.sock_senrev.send("/server_shutdown".encode())
+        """Stops the client message handler then disconnects from the client"""
+        if self.client_thread.is_running:
+            self.client_thread.stop()
+            self.sock_senrev.send("/server_conn_shutdown".encode())
+            self.logger.info(
+                f"Disconnecting to client {self.client_socket.getpeername()}"
+            )
 
-    @threaded
-    def main(self) -> None:
+    def client_msg_handler(self) -> None:
+        """Handles the Client messages"""
         self.logger.info("Starting server client listener thread")
-        while self.is_running:
+
+        while self.client_thread.is_running:
             command_in = self.sock_senrev.recv_packet().decode("utf-8")
 
             if command_in:
                 self.logger.info(f"Received command {command_in}")
                 if command_in == "/help":
-                    self.logger.info("/help command invoked!")
-
-                    self.sock_senrev.send("wasup".encode())
+                    self.handle_help()
                 elif command_in == "/exit":
                     self.logger.info("/exit command invoked!")
                     self.stop()
                 elif "/cmd" in command_in:
-                    self.logger.info("/cmd command invoked!")
-
                     arg = command_in.replace("/cmd", "", 1).strip()
-
                     if not arg:
                         self.logger.warn("No argument provided, return")
                         self.sock_senrev.send(
-                            "/cmd requires 1 argument, received zero".encode())
+                            "/cmd requires 1 argument, received zero".encode()
+                        )
                     else:
-                        self.logger.info(f"Executing {arg}!")
+                        self.logger.info(f"Executing '{arg}'!")
                         arg = arg.split(" ")
-
-                        try:
-                            process = subprocess.Popen(
-                                arg,
-                                # shell=True,
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE,
-                            )
-                            stdout, stderr = process.communicate()
-
-                            self.sock_senrev.send(stdout)
-                            if stderr.strip():
-                                self.sock_senrev.send(stderr)
-                        except Exception as e:
-                            self.logger.err(f"Exception! {type(e)}: {e}")
-                            self.sock_senrev.send(
-                                f"Caught an exception before executing process: {e}"
-                                .encode())
+                        self.handle_cmd(arg)
                 elif "/cd" in command_in:
-                    self.logger.info("/cd command invoked!")
-
                     arg = command_in.replace("/cd", "", 1).strip()
 
                     if not arg:
                         self.logger.warn("No argument provided, returning")
                         self.sock_senrev.send(
-                            "/cd requires 1 argument, received zero".encode())
+                            "/cd requires 1 argument, received zero".encode()
+                        )
                     else:
-                        self.logger.info(f"Chdir'ing to {arg}!")
+                        self.logger.info(f"Chdir'ing to '{arg}'!")
                         arg = arg.split(" ")
 
                         if len(arg) == 1:
-                            try:
-                                os.chdir(arg[0])
-                                self.current_wd = os.getcwd()
-                                self.logger.info(
-                                    f"Chdir'ed to {self.current_wd}")
-                                self.sock_senrev.send(
-                                    f"/cwd_changed {self.current_wd}".encode())
-                            except FileNotFoundError:
-                                self.sock_senrev.send(
-                                    f"Directory {arg[0]} doesn't exists!".
-                                    encode())
-                            except NotADirectoryError:
-                                self.sock_senrev.send(
-                                    f"{arg[0]} is not a directory!".encode())
-                            except Exception as e:
-                                self.logger.err(f"Exception! {type(e)}: {e}")
-                                self.sock_senrev.send(
-                                    f"Caught an exception before running `os.chdir()`: {e}"
-                                    .encode())
+                            self.handle_cd(arg[0])
                         else:
                             self.logger.warn(
                                 f"{len(arg)} argument provided, returning")
                             self.sock_senrev.send(
-                                f"/cd requires 1 argument, received {len(arg)}".
-                                encode())
+                                f"/cd requires 1 argument, received {len(arg)}".encode())
                 else:
                     self.logger.warn(f"{command_in} is not a command")
                     self.sock_senrev.send(
-                        f"{command_in} is not a valid command!".encode())
+                        f"{command_in} is not a valid command!".encode()
+                    )
 
                 self.end_block_input()
 
+    def handle_help(self) -> None:
+        """Handles the help command"""
+        self.logger.info("/help command invoked!")
+        self.sock_senrev.send("wasup".encode())
+
+    def handle_cmd(self, cmd: list[str]) -> None:
+        """Handles the cmd command
+
+        :param cmd: str[]:
+
+        """
+        self.logger.info("/cmd command invoked!")
+        try:
+            process = subprocess.Popen(
+                cmd,
+                # shell=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            )
+            stdout, stderr = process.communicate()
+
+            self.sock_senrev.send(stdout.strip())
+            if stderr.strip():
+                self.sock_senrev.send(stderr.strip())
+        except Exception as e:
+            self.logger.err(f"Exception! {type(e)}: {e}")
+            self.sock_senrev.send(
+                f"Caught an exception before executing process: {e}".encode()
+            )
+
+    def handle_cd(self, folder: str) -> None:
+        """
+        Handles the cd command
+
+        :param folder: str:
+
+        """
+        self.logger.info("/cd command invoked!")
+
+        try:
+            os.chdir(folder)
+            self.current_wd = os.getcwd()
+            self.logger.info(f"Chdir'ed to '{self.current_wd}'")
+            self.sock_senrev.send(f"/cwd_changed {self.current_wd}".encode())
+        except FileNotFoundError:
+            self.sock_senrev.send(
+                f"Directory {folder} doesn't exists!".encode())
+        except NotADirectoryError:
+            self.sock_senrev.send(f"{folder} is not a directory!".encode())
+        except Exception as e:
+            self.logger.err(f"Exception! {type(e)}: {e}")
+            self.sock_senrev.send(
+                f"Caught an exception before running `os.chdir()`: {e}".encode())
+
     def end_block_input(self) -> None:
+        """Ends the client wait for result mode"""
         self.sock_senrev.send("/end_block_input".encode())
 
 
-def start_server(ip: str, port: int) -> None:
-    my_server = Server(ip, port)
-    my_server.start()
-
-
 if __name__ == "__main__":
-    start_server("localhost", 6969)
+    if len(sys.argv) != 3:
+        print(f"{sys.argv[0]} requires 2 argument got {len(sys.argv) - 1}")
+        print(f"{sys.argv[0]}: <ip> <port>")
+    else:
+        if SERVER_REQUIRE_AUTH:
+            if SERVER_TOKEN is None or not SERVER_TOKEN.strip():
+                SERVER_TOKEN = gen_token()
+                print("New token is generated each server restart.")
+                print(
+                    "You can set the manually token by setting 'REMSHELL_TOKEN' in the env"
+                )
+
+            print(f"This is the server token: {SERVER_TOKEN}")
+
+        rem_serv = Server(str(sys.argv[1]), int(sys.argv[2]))
+        rem_serv.start()
